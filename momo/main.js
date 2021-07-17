48842c6ba8ae310c4595c858307cadfbf75cade3
kimsangwoo66 <56182236+kimsangwoo66@users.noreply.github.com>
2021-07-18 01:42:07 +0900

    JS: Navbar tab menu click event

--- momo/main.js
+++ momo/main.js
@@ -11,3 +11,19 @@ document.addEventListener("scroll", () => {
     navbar.classList.remove("navbar--dark");
   }
 });
+
+//상단바 메뉴 클릭시 그에 해당하는 섹션으로 이동
+const navbarMenu = document.querySelector(".navbar__menu");
+navbarMenu.addEventListener("click", (event) => {
+  console.log(event.target.dataset.link);
+  const target = event.target;
+  const link = target.dataset.link;
+  if (link == null) {
+    return;
+  }
+
+  console.log(event.target.dataset.link);
+  const scrollTo = document.querySelector(link);
+  //상단텝의 버튼을 눌렀을때 부드럽게 그 섹션으로 이동하게 한다.
+  scrollTo.scrollIntoView({ behavior: "smooth" });
+});
